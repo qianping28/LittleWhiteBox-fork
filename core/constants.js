f4e0183018d773624768fe87322581d5b5220b94
qianping28 <1750714670yu@gmail.com>
2026-01-22 18:41:50 +0800

    update name

--- core/constants.js
+++ core/constants.js
@@ -2,6 +2,6 @@
  * LittleWhiteBox 共享常量
  */
 
-export const EXT_ID = "LittleWhiteBox";
-export const EXT_NAME = "小白X";
+export const EXT_ID = "LittleWhiteBoxfork";
+export const EXT_NAME = "小白X改";
 export const extensionFolderPath = `scripts/extensions/third-party/${EXT_ID}`;
